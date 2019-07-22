60732e6f5a2c9ec290a3b3c47b6a35d1687bf980
namget <yeboykr@naver.com>
2019-07-22 18:38:52 +0900

    -작성중

--- app/src/main/java/com/namget/ui/second/SecondDiffUtil.java
+++ app/src/main/java/com/namget/ui/second/SecondDiffUtil.java
@@ -6,7 +6,6 @@ import com.namget.data.model.Book;
 
 class SecondDiffUtil extends DiffUtil.ItemCallback<Book> {
 
-    // Diff 유틸 조건 이렇게 서로 같으면 욕먹을 듯 두 함수 의미 찾아보고 한번 바꿔바
     @Override
     public boolean areItemsTheSame(@NonNull Book oldItem, @NonNull Book newItem) {
         return ((oldItem.getTitle().equals(newItem.getTitle()))
